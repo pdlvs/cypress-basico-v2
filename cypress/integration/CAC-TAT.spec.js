0a34322709d635bcf583d0f421fdb362e9d0b003
Patricia Dias Alves <patricia_alves.1997@hotmail.com>
2022-05-29 10:14:29 -0300

    Aula 5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -41,7 +41,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#firstName').type('Patricia')
         cy.get('#lastName').type('Alves')
         cy.get('#email').type('patriciadiasalves1@gmail.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('Teste')
         cy.contains('button', 'Enviar').click()
 
@@ -105,13 +105,21 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         .should('have.value', 'feedback')
     })
 
-    it.only('marca cada tipo de atendimento', function(){
+    it('marca cada tipo de atendimento', function(){
         cy.get('input[type="radio"]')
         .should('have.length', 3)
         .each(function($radio){
             cy.wrap($radio).check()
             cy.wrap($radio).should('be.checked')
         })
+    })
+    
+    it('marca ambos checkboxes, depois desmarca o último', function(){
+        cy.get('input[type="checkbox"]')
+        .check()
+        .last()
+        .uncheck()
+        .should('not.be.checked')
     })
 })
 
